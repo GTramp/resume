626e4fb671a41e330f0c3fc6d39f12597b009b1a
command <wg_workshop@outlook.com>
2018-01-25 18:59:45 +0800

    搭建基本框架

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -2,24 +2,31 @@
  * @Author: command
  * @Date: 2018-01-25 16:58:25 
  * @Last Modified by: commad
- * @Last Modified time: 2018-01-25 17:42:44
+ * @Last Modified time: 2018-01-25 18:45:46
  */
 import React from 'react'
 import './style.css'
 
 import Header from '../header'
+import SideBar from '../sidebar'
 
 export default class App extends React.Component {
   /**
    * render
-   * 
-   * @returns 
+   *
+   * @returns
    * @memberof App
    */
   render() {
     return (
       <div id="app">
+        {/* header */}
         <Header />
+        {/* container */}
+        <div className="container">
+          {/* sidebar */}
+          <SideBar />
+        </div>
       </div>
     )
   }
---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,25 @@
+/*
+ * @Author: command
+ * @Date: 2018-01-25 18:42:22 
+ * @Last Modified by: commad
+ * @Last Modified time: 2018-01-25 18:43:55
+ */
+
+import React from 'react'
+import './style.css'
+
+export default class SideBar extends React.Component {
+  /**
+   * render
+   *
+   * @returns
+   * @memberof SideBar
+   */
+  render() {
+    return (
+      <div id="sidebar">
+        <span>sidebar</span>
+      </div>
+    )
+  }
+}
